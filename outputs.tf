a6dea8c0c4af1032b12cdeb5d6142fc35bb30384
Richard <richardhurtado07@gmail.com>
2025-09-24 00:01:47 +0200

    fix: Correct module reference in outputs.tf
    - Change module.vpc to module.private_vpc to match main.tf
    - Add additional useful outputs for EC2 and EKS
    - Fix undeclared module reference error

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.private_vpc.vpc_id
 }
 
 output "subnet_id" {
@@ -8,4 +8,16 @@ output "subnet_id" {
 
 output "ec2_instance_id" {
   value = module.ec2_instance.instance_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2_instance.public_ip
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
 }
