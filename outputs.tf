d0ded70964b70e240e6a2f9ee76dfe382cf88e8c
Richard <richardhurtado07@gmail.com>
2025-09-24 00:02:52 +0200

    fix: Remove duplicate EKS outputs from outputs.tf
    - Remove eks_cluster_name and eks_cluster_endpoint from outputs.tf
    - Keep EKS outputs only in eks.tf where they belong
    - Fix duplicate output definition error

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,3 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = module.ec2_instance.public_ip
 }
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.this.name
-}
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
