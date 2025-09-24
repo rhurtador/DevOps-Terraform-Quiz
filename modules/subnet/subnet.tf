7d88ee2827f3499f8b8a1fd7d1cd01e5afc0c636
Richard <richardhurtado07@gmail.com>
2025-09-24 12:54:42 +0200

    edit modulo manualy

--- modules/subnet/subnet.tf
+++ modules/subnet/subnet.tf
@@ -5,8 +5,14 @@ resource "aws_subnet" "main" {
   tags                    = var.subnet_tags
 }
 
+# resource "aws_route_table_association" "public_assoc" {
+#   count          = var.route_table_id == "" ? 0 : 1
+#   subnet_id      = aws_subnet.main.id
+#   route_table_id = var.route_table_id
+# }
+
 resource "aws_route_table_association" "public_assoc" {
-  count          = var.route_table_id == "" ? 0 : 1
-  subnet_id      = aws_subnet.main.id
-  route_table_id = var.route_table_id
+  for_each       = var.route_table_id != "" ? toset([var.route_table_id]) : []
+  subnet_id      = aws_subnet.public.id
+  route_table_id = each.value
 }
