8132a4d3e60256db3c98c1f8660fc8ff795a73fb
Richard <richardhurtado07@gmail.com>
2025-09-30 14:57:22 +0200

    fix(ec2): usar key pair existente por nombre y no crear/importar

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "main" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.main.key_name
+  key_name               = var.ec2_key_name
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
 
--- modules/ec2/keypair.tf
+++ modules/ec2/keypair.tf
@@ -1,18 +1,1 @@
-# Crear Key Pair
-# Genera una clave privada
-resource "tls_private_key" "main" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
 
-# Crea el par de claves en AWS
-resource "aws_key_pair" "main" {
-  key_name   = var.ec2_key_name
-  public_key = tls_private_key.main.public_key_openssh
-}
-
-# Guardar la clave privada en un archivo local
-resource "local_file" "private_key" {
-  content  = tls_private_key.main.private_key_pem
-  filename = "${path.module}/id_rsa"
-}
