1952bc599b70916fa8c157ccb85c283362c4fdd6
Richard <richardhurtado07@gmail.com>
2025-09-30 12:39:38 +0200

    fix(terraform): declarar variables requeridas en módulos subnet/NSG/VPC/EC2

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,40 +1,40 @@
-# variable "ami_id" {
-#   description = "AMI ID de la instancia EC2"
-#   type        = string
-# }
+variable "ami_id" {
+  description = "AMI ID de la instancia EC2"
+  type        = string
+}
 
-# variable "instance_type" {
-#   description = "Tipo de instancia EC2"
-#   type        = string
-# }
+variable "instance_type" {
+  description = "Tipo de instancia EC2"
+  type        = string
+}
 
-# variable "ec2_key_name" {
-#   description = "Nombre del Key Pair para la instancia EC2"
-#   type        = string
-# }
+variable "ec2_key_name" {
+  description = "Nombre del Key Pair para la instancia EC2"
+  type        = string
+}
 
-# variable "subnet_id" {
-#   description = "ID de la Subnet para la EC2"
-#   type        = string
-# }
+variable "subnet_id" {
+  description = "ID de la Subnet para la EC2"
+  type        = string
+}
 
-# variable "security_group_ids" {
-#   description = "Lista de SGs a asociar a la EC2"
-#   type        = list(string)
-# }
+variable "security_group_ids" {
+  description = "Lista de SGs a asociar a la EC2"
+  type        = list(string)
+}
 
-# variable "ec2_tags" {
-#   description = "Etiquetas para la instancia EC2"
-#   type        = map(string)
-# }
+variable "ec2_tags" {
+  description = "Etiquetas para la instancia EC2"
+  type        = map(string)
+}
 
-# variable "public_key_path" {
-#   description = "Ruta al archivo que contiene tu clave pública"
-#   type        = string
-# }
+variable "public_key_path" {
+  description = "Ruta al archivo que contiene tu clave pública"
+  type        = string
+}
 
-# variable "ec2_name" {
-#   description = "Nombre de la instancia EC2"
-#   type        = string
-#   default     = "default-ec2-instance"
-# }
+variable "ec2_name" {
+  description = "Nombre de la instancia EC2"
+  type        = string
+  default     = "default-ec2-instance"
+}
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,33 +1,31 @@
-# variable "vpc_id" {
-#   description = "ID de la VPC donde se creará la subred"
-#   type        = string
-# }
+variable "vpc_id" {
+  description = "ID de la VPC donde se creará la subred"
+  type        = string
+}
 
-# variable "cidr_block" {
-#   description = "Bloque CIDR para la subred"
-#   type        = string
-# }
+variable "cidr_block" {
+  description = "Bloque CIDR para la subred"
+  type        = string
+}
 
-# variable "subnet_tags" {
-#   description = "Etiquetas para la subred"
-#   type        = map(string)
-# }
+variable "subnet_tags" {
+  description = "Etiquetas para la subred"
+  type        = map(string)
+}
 
-# variable "subnet_name" {
-#   description = "Nombre de la Subnet"
-#   type        = string
-#   default     = "default-subnet"
-# }
+variable "subnet_name" {
+  description = "Nombre de la Subnet"
+  type        = string
+  default     = "default-subnet"
+}
 
-# # variable "route_table_id" {
-# #   description = "ID de la tabla de rutas a asociar (opcional)"
-# #   type        = string
-# #   default     = ""
-# # }
-# variable "route_table_id" {
-#   type = string
-# }
-# variable "availability_zone" {
-#   type    = string
-#   default = null
-# }
+variable "route_table_id" {
+  description = "ID de la tabla de rutas a asociar"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Zona de disponibilidad para la subred"
+  type        = string
+  default     = null
+}
